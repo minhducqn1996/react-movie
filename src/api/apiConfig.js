619f583f3f13ac044ea742c16d5f4b7fd1113095
minhducqn1996 <minhducqn1996@gmail.com>
2022-01-27 04:10:25 +0700

    first commit

---
+++ src/api/apiConfig.js
@@ -0,0 +1,8 @@
+const apiConfig = {
+    baseUrl: 'https://api.themoviedb.org/3/',
+    apiKey: 'e77052225d46a59ada3d79328a93e85d',
+    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
+}
+
+export default apiConfig;
